0f49dd7e4d92ad5354a91eaab363692faf79a9a0
zali09 <zali09_ind@yahoo.co.in>
2018-02-25 11:46:12 +0800

    Updated Hello file

--- Hello.java
+++ Hello.java
@@ -1,10 +1,3 @@
-*Hello
-public class Hello{
-
-   public static void main (String[] args){
-    for(int i=1;i<=10;i++){
-     System.out.println("Hello World...."+i);
-}
-
-}
-}
+echo "Hello ..."
+dir
+echo  "Thanks"
